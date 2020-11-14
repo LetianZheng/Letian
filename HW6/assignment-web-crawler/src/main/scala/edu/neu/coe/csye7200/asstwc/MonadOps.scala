fbc765c13b624269af65f60f482b4eaab4f5f697
Letian <zheng.le@husky.neu.edu>
2020-11-14 12:15:38 +0800

    Web Crawler Assignment

---
+++ HW6/assignment-web-crawler/src/main/scala/edu/neu/coe/csye7200/asstwc/MonadOps.scala
@@ -0,0 +1,156 @@
+package edu.neu.coe.csye7200.asstwc
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
+import scala.util._
+
+/**
+ * @author scalaprof
+ */
+//noinspection ScalaDeprecation
+object MonadOps {
+
+  def flatten[X](xyf: Future[Try[X]])(implicit executor: ExecutionContext): Future[X] = for (xy <- xyf; x <- asFuture(xy)) yield x
+
+  def flatten[X](xfy: Try[Future[X]]): Future[X] =
+    xfy match {
+      case Success(xf) => xf
+      case Failure(e) => Future.failed(e)
+    }
+
+  def flatten[X](xsfs: Seq[Future[Seq[X]]])(implicit ec: ExecutionContext): Future[Seq[X]] = Future.sequence(xsfs) map {
+    _ flatten
+  }
+
+  def flattenRecover[X](esf: Future[Seq[Either[Throwable, Seq[X]]]], f: => Throwable => Unit)(implicit executor: ExecutionContext): Future[Seq[X]] = {
+    def filter(uses: Seq[Either[Throwable, Seq[X]]]): Seq[X] = {
+      val uses2 = for {use <- uses; if (use match {
+        case Left(x) => f(x); false;
+        case _ => true
+      })} yield use
+      val uss = for {use <- uses2; uso = sequence(use); us <- uso} yield us
+      uss flatten
+    }
+    for {es <- esf; e = filter(es)} yield e
+  }
+
+  def flatten[K, V](voKm: Map[K, Option[V]]): Map[K, V] = for ((k, vo) <- voKm; v <- vo) yield k -> v
+
+
+  def asFuture[X](xy: Try[X]): Future[X] = xy match {
+    case Success(s) => Future.successful(s)
+    case Failure(e) => Future.failed(e)
+  }
+
+  def sequence[X](xy: Try[X]): Either[Throwable, X] =
+    xy match {
+      case Success(s) => Right(s)
+      case Failure(e) => Left(e)
+    }
+
+  def sequence[X](xf: Future[X])(implicit executor: ExecutionContext): Future[Either[Throwable, X]] =
+    xf transform( { s => Right(s) }, { f => f }) recoverWith[Either[Throwable, X]] { case f => Future(Left(f)) }
+
+  // Hint: write as a for-comprehension, using the method sequence (above).
+  // 6 points.
+  def sequence[X](xfs: Seq[Future[X]])(implicit executor: ExecutionContext): Seq[Future[Either[Throwable, X]]] =
+    for(xf<-xfs) yield sequence(xf) // TO BE IMPLEMENTED
+
+  /**
+   * Sequence the Seq of Try of X into a Try of Seq of X such that if any of the input elements is a Failure,
+   * then only that one (the first) will be returned as a Failure.
+   * Subsequent failures will be quietly ignored.
+   *
+   * @param xys a Seq of Try of X.
+   * @tparam X the underlying type.
+   * @return a Try of Seq of X.
+   */
+    // TODO Fix deprecation
+  def sequence[X](xys: Seq[Try[X]]): Try[Seq[X]] = (Try(Seq[X]()) /: xys) (combineStrict(_, _)(_ => ()))
+
+  // TODO Fix deprecation
+  def sequenceWithLogging[X](xys: Seq[Try[X]])(onSubsequentFailure: Throwable => Unit): Try[Seq[X]] = (Try(Seq[X]()) /: xys) (combineStrict(_, _)(onSubsequentFailure))
+
+  // TODO Fix deprecation
+  def sequence[X](xys: Stream[Try[X]]): Try[Stream[X]] = (Try(Stream[X]()) /: xys) {
+    (xsy, xy) => for (xs <- xsy; x <- xy) yield xs :+ x
+  }
+
+  // TODO Fix deprecation
+  def sequenceForgivingWithLogging[X](xys: Seq[Try[X]])(onFailure: Throwable => Unit): Try[Seq[X]] = (Try(Seq[X]()) /: xys) (combineLax(_, _)(onFailure))
+
+  // TODO Fix deprecation
+  def sequenceForgiving[X](xys: Seq[Try[X]]): Try[Seq[X]] = (Try(Seq[X]()) /: xys) (combineLax(_, _)(e => System.err.println(s"ignoring exception ${e.getLocalizedMessage}")))
+
+  // TODO Fix deprecation
+  def sequence[X](xos: Seq[Option[X]]): Option[Seq[X]] = (Option(Seq[X]()) /: xos) {
+    (xso, xo) => for (xs <- xso; x <- xo) yield xs :+ x
+  }
+
+  // Hint: this one is a little more tricky. Remember what I mentioned about Either not being a pure monad -- it needs projecting
+  // 7 points.
+  def sequence[X](xe: Either[Throwable, X]): Option[X] = xe.right.toOption // TO BE IMPLEMENTED
+
+  def zip[A, B](ao: Option[A], bo: Option[B]): Option[(A, B)] = for (a <- ao; b <- bo) yield (a, b)
+
+  def optionToTry[X](xo: Option[X], t: => Throwable): Try[X] = Try(xo.get).recoverWith { case _: java.util.NoSuchElementException => Failure[X](t) }
+
+  def optionToTry[X](xo: Option[X]): Try[X] = Try(xo.get)
+
+  def map2[T, U](ty1: Try[T], ty2: Try[T])(f: (T, T) => U): Try[U] = for {t1 <- ty1; t2 <- ty2} yield f(t1, t2)
+
+  def liftTry[T, U](f: T => U): Try[T] => Try[U] = _ map f
+
+  def liftOption[T, U](f: T => U): Option[T] => Option[U] = _ map f
+
+  def liftFuture[T, U](f: T => U)(implicit executor: ExecutionContext): Future[T] => Future[U] = _ map f
+
+  /**
+   * Strict form of combine.
+   * If xsy is a Failure, then the result will be the same Failure, while a successful xy will be ignored and
+   * a failing xy will be passed to onFailure.
+   * Subsequent Failures will be processed according to the onFailure function.
+   *
+   * @param xsy       a Try of Seq of X (the accumulator).
+   * @param xy        a Try of X (the addend).
+   * @param onFailure a function to process a failing xy if xsy is a Failure.
+   * @tparam X the underlying type.
+   * @return a Try of Seq of X.
+   */
+  private def combineStrict[X](xsy: Try[Seq[X]], xy: Try[X])(onFailure: Throwable => Unit): Try[Seq[X]] = xsy match {
+    case Success(xs) =>
+      xy match {
+        case Success(x) => Success(xs :+ x)
+        case Failure(e) => Failure(e)
+      }
+    case Failure(e) =>
+      xy match {
+        case Success(_) =>
+        case Failure(z) => onFailure(z)
+      }
+      Failure(e)
+  }
+
+  /**
+   * Lax form of combine.
+   * If xsy is a Success, then the result will depend on xy:
+   * If xy is a Success, then the result will be the concatenation of the underlying elements.
+   * If xy is a Failure, then its exception will be processed by onFailure, and xsy will be returned unchanged.
+   * a failing xy will be passed to onFailure.
+   * Subsequent Failures will be processed according to the onFailure function.
+   *
+   * @param xsy       a Try of Seq of X (the accumulator).
+   * @param xy        a Try of X (the addend).
+   * @param onFailure a function to process a failing xy if xsy is a Failure.
+   * @tparam X the underlying type.
+   * @return a Try of Seq of X.
+   */
+  private def combineLax[X](xsy: Try[Seq[X]], xy: Try[X])(onFailure: Throwable => Unit): Try[Seq[X]] = xsy match {
+    case Success(xs) =>
+      xy match {
+        case Success(x) => Success(xs :+ x)
+        case Failure(e) => onFailure(e); xsy
+      }
+    case Failure(e) => Failure(new Exception(s"combineLax: logic error: Failure(${e.getLocalizedMessage})"))
+  }
+}
---
+++ HW6/assignment-web-crawler/src/main/scala/edu/neu/coe/csye7200/asstwc/WebCrawler.scala
@@ -0,0 +1,94 @@
+package edu.neu.coe.csye7200.asstwc
+
+import java.net.URL
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.io.{BufferedSource, Source}
+import scala.language.postfixOps
+import scala.util._
+import scala.util.control.NonFatal
+import scala.xml.Node
+
+/**
+ * @author scalaprof
+ */
+object WebCrawler extends App {
+
+  def getURLContent(u: URL): Future[String] =
+    for {
+      s <- MonadOps.asFuture(SourceFromURL(u))
+      w <- MonadOps.asFuture(sourceToString(s, s"Cannot read from source at $u"))
+    } yield w
+
+  def wget(u: URL): Future[Seq[URL]] = {
+    // Hint: write as a for-comprehension, using the method createURL(Option[URL], String) to get the appropriate URL for relative links
+    // 16 points.
+    def getURLs(ns: Node): Seq[Try[URL]] = for{
+      n<-ns\\"a"
+      url = n \@ "href"
+    } yield createURL(Option(u),url)
+      // TO BE IMPLEMENTED
+
+    def getLinks(g: String): Try[Seq[URL]] = {
+      val ny = HTMLParser.parse(g) recoverWith { case f => Failure(new RuntimeException(s"parse problem with URL $u: $f")) }
+      for (n <- ny; z <- MonadOps.sequence(getURLs(n))) yield z
+    }
+    // Hint: write as a for-comprehension, using getURLContent (above) and getLinks above. You might also need MonadOps.asFuture
+    // 9 points.
+    for { g <- getURLContent(u); f<- MonadOps.asFuture(getLinks(g))} yield f // TO BE IMPLEMENTED
+  }
+
+  def wget(us: Seq[URL]): Future[Seq[Either[Throwable, Seq[URL]]]] = {
+    val us2 = us.distinct take 10
+    // Hint: Use wget(URL) (above). MonadOps.sequence and Future.sequence are also available to you to use.
+    // 15 points. Implement the rest of this, based on us2 instead of us.
+    // TO BE IMPLEMENTED
+    val x = for { u <- us2 } yield wget(u)
+    Future.sequence(MonadOps.sequence(x))
+  }
+
+  def crawler(depth: Int, us: Seq[URL]): Future[Seq[URL]] = {
+    def inner(urls: Seq[URL], depth: Int, accum: Seq[URL]): Future[Seq[URL]] =
+      if (depth > 0)
+        for (us <- MonadOps.flattenRecover(wget(urls), { x => System.err.println(s"""crawler: ignoring exception $x ${if (x.getCause != null) " with cause " + x.getCause else ""}""") }); r <- inner(us, depth - 1, accum ++: urls)) yield r
+      else
+        Future.successful(accum)
+
+    inner(us, depth, Nil)
+  }
+
+  println(s"web reader: ${args.toList}")
+  val uys = for (arg <- args toList) yield getURL(arg)
+  val s = MonadOps.sequence(uys)
+  s match {
+    case Success(z) =>
+      println(s"invoking crawler on $z")
+      val f = crawler(2, z)
+      Await.ready(f, Duration("60 second"))
+      for (x <- f) println(s"Links: $x")
+    case Failure(z) => println(s"failure: $z")
+  }
+
+  private def sourceToString(source: BufferedSource, errorMsg: String): Try[String] =
+    try Success(source mkString) catch {
+      case NonFatal(e) => Failure(WebCrawlerException(errorMsg, e))
+    }
+
+  private def getURL(resource: String): Try[URL] = createURL(null, resource)
+
+  private def createURL(context: Option[URL], resource: String): Try[URL] =
+    try Success(new URL(context.orNull, resource)) catch {
+      case NonFatal(e) =>
+        val message: String = s"""Bad URL: ${if (context.isDefined) "context: " + context else ""} resource=$resource"""
+        Failure(WebCrawlerException(message, e))
+    }
+
+  private def SourceFromURL(resource: URL): Try[BufferedSource] =
+    try Success(Source.fromURL(resource)) catch {
+      case NonFatal(e) => Failure(WebCrawlerException(s"""Cannot get source from URL: $resource""", e))
+    }
+}
+
+case class WebCrawlerException(url: String, cause: Throwable) extends Exception(s"Web Crawler could not decode URL: $url", cause)
